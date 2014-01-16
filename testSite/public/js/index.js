5e25e786cccb372c7bc896dc39c4c0bca045b4f4
Alec Heifetz <aheifetz@mit.edu>
2014-01-16 18:31:11 -0500

    Merging modules into index for landing page

--- testSite/public/js/index.js
+++ testSite/public/js/index.js
@@ -7,6 +7,33 @@ $(document).ready(function(req, res) {
 		},
 	});
 	$('.typeahead.input-sm').siblings('input.tt-hint').addClass('hint-small');
-	$('.typeahead.input-lg').siblings('input.tt-hint').addClass('hint-large');                                                   
+	$('.typeahead.input-lg').siblings('input.tt-hint').addClass('hint-large');  
+	//$('.tt-query').css('background-color','#fff');                                                 
 });
 
+$(window).load(function() {
+	$('myModal').modal('show');
+})
+
+function process(data, status) {
+	console.log(data);
+	if(data == 'SUCCESS') {
+		var profile_url = window.location.protocol + "//" + window.location.host + "/profile";
+		window.location.href = profile_url;
+	}
+}
+
+$(document).ready(function(req, res) {
+	$('#loginAccount').submit(function(e) {
+		e.preventDefault();
+		var input = $(this).serialize();
+		$.post('/loginAccount', input, process);
+	});
+
+	$('#createAccount').submit(function(e) {
+		e.preventDefault();
+		var input = $(this).serialize();
+		$.post('createAccount', input, process);
+
+	})
+});
